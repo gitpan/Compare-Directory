71436d5372422a59b41ce32dc0b3a66af1892c8b
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2010-11-15 20:33:32 +0000

    Import of MANWAR/Compare-Directory-0.04 from CPAN.
    gitpan-cpan-distribution: Compare-Directory
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         MANWAR/Compare-Directory-0.04.tar.gz
    gitpan-cpan-author:       MANWAR
    gitpan-cpan-maturity:     released

--- lib/Compare/Directory.pm
+++ lib/Compare/Directory.pm
@@ -8,11 +8,11 @@ Compare::Directory - A very simple utility to compare directory.
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 use Carp;
 use CAM::PDF;
